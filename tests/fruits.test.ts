a61e99d25f03a87eb570859f4450fcec24e3122a
Vittorio <vittfassano@gmail.com>
2023-02-02 11:44:43 -0300

    test getSpecifFruit

--- tests/fruits.test.ts
+++ tests/fruits.test.ts
@@ -5,17 +5,28 @@ import supertest from "supertest";
 const api = supertest(app);
 
 describe("fruits test", () => {
-    it("testando getAllFruits", async () => {
-        const resultado = await api.get('/fruits');
-        console.log(resultado)
-    })
-
-    it("testando createFruit", async () => {
+    it("testing createFruit", async () => {
         const body: FruitInput = {
             name: "Banana",
             price: 10
         }
         const resultado = await api.post('/fruits').send(body);
-        console.log(resultado)
+        expect(resultado.status).toBe(201);
+        console.log(resultado.status);
+    })
+
+    it("testing getAllFruits", async () => {
+        const resultado = await api.get('/fruits');
+        expect(resultado.status).toBe(200);
+        console.log(resultado.status);
+    })
+
+    it("testing getSpecificFruit", async () => {
+        const params = {
+            id: 1,
+        }
+        const resultado = await api.get(`/fruits/${params.id}`);
+        expect(resultado.status).toBe(200);
+        console.log(resultado.status);
     })
 })
