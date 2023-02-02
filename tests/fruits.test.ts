47b5a6c1bc73f39f992159b5091d7aae4afd5e7c
Vittorio <vittfassano@gmail.com>
2023-02-02 11:23:50 -0300

    feat: test createFruit

--- tests/fruits.test.ts
+++ tests/fruits.test.ts
@@ -5,8 +5,17 @@ import supertest from "supertest";
 const api = supertest(app);
 
 describe("fruits test", () => {
-    it("testando get all", async () => {
+    it("testando getAllFruits", async () => {
         const resultado = await api.get('/fruits');
+        console.log(resultado)
+    })
+
+    it("testando createFruit", async () => {
+        const body: FruitInput = {
+            name: "Banana",
+            price: 10
+        }
+        const resultado = await api.post('/fruits').send(body);
         console.log(resultado)
     })
 })
